e607ecdb9005e512a0d6655999dd1ed01d80f78f
krovyakovmikhail <77331950+krovyakovmikhail@users.noreply.github.com>
2021-08-19 08:01:01 +0300

    create a new progect

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tankgeddon
+
+Developed with Unreal Engine 4
---
+++ Source/Tankgeddon/TankgeddonGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankgeddonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKGEDDON_API ATankgeddonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
